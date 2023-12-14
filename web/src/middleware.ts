f9571d168bfef653e7f48b52438b7ecacb943bb4
don.cryptus@gmail.com <don.cryptus@gmail.com>
2023-12-14 23:50:49 +0100

    easy

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -5,6 +5,7 @@ export const LOCALES = ["de", "en"] as const;
 export default createMiddleware({
   locales: LOCALES,
   defaultLocale: "de",
+  localePrefix: "as-needed",
   localeDetection: false,
 });
 
