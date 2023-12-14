bc4e5a5fc9225568dee4e86d3369cf4649f20290
don.cryptus@gmail.com <don.cryptus@gmail.com>
2023-12-14 23:49:27 +0100

    easy

--- web/src/app/[locale]/page.tsx
+++ web/src/app/[locale]/page.tsx
@@ -1,12 +1,8 @@
 import { ThemeSwitcher } from "@/components/utils/ThemeSwitcher";
-import { GET_ALL_USERS } from "@/documents/queries/user";
-import { useGqlQuery } from "@/fetcher";
 import { Link } from "@/navigation";
 import { Button } from "@nextui-org/react";
 
 export default function Home() {
-  const { data } = useGqlQuery(GET_ALL_USERS);
-  data?.getAllUsersUser?.[0].id
   return (
     <main className="flex min-h-screen items-center justify-center space-x-5">
       <Link href="/login">
