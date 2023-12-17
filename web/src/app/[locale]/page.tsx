a086a61e74efb669a0363d1fd067cb487ebd804e
don.cryptus <don.cryptus@gmail.com>
2023-12-18 00:48:42 +0100

    easy

--- web/src/app/[locale]/page.tsx
+++ web/src/app/[locale]/page.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@nextui-org/button";
-
 export default function Home() {
   return (
     <main className="flex h-[90vh] items-center justify-center space-x-5">
-      <Button>Test</Button>
+      <div>
+        {/* Gradient Heading */}
+        <h1 className="bg-gradient-to-r from-yellow-400 to-orange-600 bg-clip-text pb-3 text-5xl font-bold text-transparent dark:from-yellow-600 dark:to-orange-800">
+          Meal Manager
+        </h1>
+        {/* Description Text */}
+        <p className=" text-lg text-gray-400 dark:text-gray-300">
+          Your ultimate companion for managing daily meals efficiently.
+        </p>
+      </div>
     </main>
   );
 }
