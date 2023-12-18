5211d9a2ba30ab0ce6071e04ba6e0c4fd5947cdf
don.cryptus <don.cryptus@gmail.com>
2023-12-19 00:49:49 +0100

    easy

--- web/src/app/[locale]/(home)/page.tsx
+++ web/src/app/[locale]/(home)/page.tsx
@@ -8,19 +8,16 @@ export default function Home() {
   const t = useTranslations<"Main">();
   return (
     <>
-      <div className="relative flex min-h-[calc(100svh-4rem)] flex-col items-center pt-12">
-
+      <div className="relative flex min-h-[calc(100svh-4rem)] flex-col items-center px-6 pt-12">
         <LogoSwitch />
         <h1 className="bg-gradient-to-r from-red-600 to-blue-600 bg-clip-text text-3xl font-bold leading-relaxed text-transparent dark:from-secondary dark:to-primary">
           {process.env.NEXT_PUBLIC_BRAND_NAME}
         </h1>
-        <h5 className="max-w-2xl text-center text-6xl font-bold">
-          Empowering Your Kitchen with Smart Technology
+        <h5 className="max-w-2xl text-center text-3xl font-bold md:text-6xl">
+          {t("HEADER")}
         </h5>
         <p className="my-10 max-w-2xl text-center text-lg text-gray-700 dark:text-gray-300">
-          Transform your culinary journey with{" "}
-          {process.env.NEXT_PUBLIC_BRAND_NAME}, where technology meets
-          nutrition.
+          {t("SUBHEADER", { brand: process.env.NEXT_PUBLIC_BRAND_NAME })}
         </p>
 
         <div className="mt-5 flex w-full items-center justify-center space-x-10">
