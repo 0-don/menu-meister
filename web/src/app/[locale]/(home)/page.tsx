77657a1cb746a09f455402ca494f1f2d599f4361
don.cryptus <don.cryptus@gmail.com>
2023-12-18 15:51:24 +0100

    easy

--- web/src/app/[locale]/(home)/page.tsx
+++ web/src/app/[locale]/(home)/page.tsx
@@ -3,9 +3,9 @@ import { Button } from "@nextui-org/button";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations();
+  const t = useTranslations<"Main">();
   return (
-    <main className="m-auto flex h-full items-center justify-center space-x-5">
+    <main className="flex min-h-full items-center justify-center space-x-5">
       <div className="space-y-2">
         {/* Gradient Heading */}
         <h1 className="bg-gradient-to-r from-yellow-400 to-orange-600 bg-clip-text pb-3 text-5xl font-bold text-transparent dark:from-yellow-600 dark:to-orange-800">
@@ -13,17 +13,17 @@ export default function Home() {
         </h1>
         {/* Description Text */}
         <p className="text-lg text-gray-400 dark:text-gray-300">
-          Your ultimate companion for managing daily meals efficiently.
+          {t("DESCRIPTION")}
         </p>
         <div className="space-x-5">
           <Link href="/login">
             <Button size="lg" color="warning">
-              Login
+              {t("LOGIN_BUTTON")}
             </Button>
           </Link>
           <Link href="/register">
             <Button size="lg" color="default">
-              Sign Up
+              {t("REGISTER_BUTTON")}
             </Button>
           </Link>
         </div>
