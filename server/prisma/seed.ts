34cccf962fcdbde0bc26acbaf7b49716a0377655
don.cryptus <don.cryptus@gmail.com>
2023-12-20 00:24:10 +0100

    easy

---
+++ server/prisma/seed.ts
@@ -0,0 +1,59 @@
+import { faker } from "@faker-js/faker";
+import { PrismaClient, UserRoleName } from "@prisma/client";
+import argon2 from "argon2";
+const prisma = new PrismaClient();
+
+const seed = async () => {
+  await createUser({
+    email: "admin@admin.de",
+    password: "!admin",
+    roles: ["USER", "ADMIN"],
+  });
+};
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+const createUser = async ({
+  roles,
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+  roles: (keyof typeof UserRoleName)[];
+}) => {
+  const user = await prisma.user.create({
+    data: {
+      email,
+      username: faker.internet.userName(),
+      password: await argon2.hash(password),
+      lastOnline: faker.date.past(),
+    },
+  });
+
+  await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      createdBy: user.id,
+      updatedBy: user.id,
+    },
+  });
+
+  for (const role of roles) {
+    await prisma.userRole.create({
+      data: {
+        userId: user.id,
+        name: role,
+        createdBy: user.id,
+        updatedBy: user.id,
+      },
+    });
+  }
+};
