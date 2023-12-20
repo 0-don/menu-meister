fe77c9814e24675f9f62bec562bd2d7bb4443a81
don.cryptus <don.cryptus@gmail.com>
2023-12-20 21:23:57 +0100

    easy

--- web/src/app/[locale]/(auth)/logout/page.tsx
+++ web/src/app/[locale]/(auth)/logout/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
+import { LOGOUT } from "@/documents/mutation/auth";
+import { useGqlMutation } from "@/fetcher";
 import { useRouter } from "@/navigation";
+import { useEffect } from "react";
 
 export default function LogoutPage() {
   const router = useRouter();
-  // const { mutateAsync: logoutAsync } = useGqlMutation(LOGOUT);
+  const { mutateAsync: logoutAsync } = useGqlMutation(LOGOUT);
 
-  // useEffect(() => {
-  //   const logout = async () => {
-  //     await logoutAsync({});
-  //     // router.push("/");
-  //   };
-  //   logout();
-  // }, []);
+  useEffect(() => {
+    const logout = async () => {
+      await logoutAsync({});
+      router.push("/");
+    };
+    logout();
+  }, []);
 
   return <></>;
 }
