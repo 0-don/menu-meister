38f19ca3abd7a35fed6226928c64543b3ff02e48
don.cryptus <don.cryptus@gmail.com>
2023-12-21 03:40:34 +0100

    easy

--- web/src/app/[locale]/about/page.tsx
+++ web/src/app/[locale]/about/page.tsx
@@ -5,7 +5,6 @@ import { useMeHook } from "@/components/hooks/useMeHook";
 interface AboutPageProps {}
 
 export default function AboutPage({}: AboutPageProps) {
-  // const { me } = useMeHook();
-  // console.log(me);
+
   return <></>;
 }
