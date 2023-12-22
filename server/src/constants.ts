ed7519cbf67f64d337979c048d54ed458c0996c4
don.cryptus <don.cryptus@gmail.com>
2023-12-22 20:10:03 +0100

    easy

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -4,13 +4,9 @@ import "dotenv/config";
 import psl from "psl";
 
 export const PORT = process.env?.PORT || 4000;
-
 export const TOKEN_EXPIRES_IN = 60 * 60 * 24 * 30; // 30 days
-
 export const COOKIE_NAME = "token" as const;
-
 export const JWT_SERVICE = new JwtService();
-
 export const CORS_DOMAINS = process.env.CORS_ORIGIN.replace(/ /g, "").split(
   ",",
 );
@@ -18,17 +14,21 @@ export const CORS_DOMAINS = process.env.CORS_ORIGIN.replace(/ /g, "").split(
 export const COOKIE_SERIALIZE_OPTIONS = (
   domain?: string,
 ): CookieSerializeOptions => {
+  const isProduction = process.env.NODE_ENV === "production";
+  let effectiveDomain: string | undefined = undefined;
+
+  if (domain && isProduction) {
+    effectiveDomain = `.${psl.parse(new URL(domain).hostname).domain}`;
+  } else if (isProduction && CORS_DOMAINS.length) {
+    effectiveDomain = `.${psl.parse(new URL(CORS_DOMAINS[0]).hostname).domain}`;
+  }
+
   return {
     httpOnly: true,
     path: "/",
     maxAge: TOKEN_EXPIRES_IN,
-    sameSite: process.env.NODE_ENV === "production" ? "lax" : "none",
-    secure: true, //process.env.NODE_ENV === 'production' ? true : false, // cookie only works in https
-    domain:
-      domain && process.env.NODE_ENV === "production"
-        ? `.${psl.parse(new URL(domain).hostname).domain}`
-        : process.env.NODE_ENV === "production" && CORS_DOMAINS.length > 0
-          ? `.${psl.parse(new URL(CORS_DOMAINS[0]).hostname).domain}`
-          : undefined,
+    sameSite: isProduction ? "lax" : "none",
+    secure: true,
+    domain: effectiveDomain,
   };
 };
