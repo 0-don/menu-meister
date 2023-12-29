36c67432b921ddb2bf3c9a1f98145aa2b4ae8113
don.cryptus <don.cryptus@gmail.com>
2023-12-30 00:29:17 +0100

    easy

--- web/src/components/tree/SortableTree.tsx
+++ web/src/components/tree/SortableTree.tsx
@@ -1,36 +1,24 @@
 import {
   DndContext,
-  DragEndEvent,
-  DragMoveEvent,
-  DragOverEvent,
   DragOverlay,
-  DragStartEvent,
-  KeyboardSensor,
-  MeasuringStrategy,
-  PointerSensor,
   UniqueIdentifier,
   closestCenter,
-  useSensor,
-  useSensors,
 } from "@dnd-kit/core";
 import {
   SortableContext,
   arrayMove,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
-import { Props } from "next/script";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useMemo, useState } from "react";
 import { createPortal } from "react-dom";
 import { SortableTreeItem } from "./SortableTreeItem";
-import { sortableTreeKeyboardCoordinates } from "./keyboardCoordinates";
-import type { FlattenedItem, SensorContext, TreeItems } from "./types";
+import type { FlattenedItem, TreeItems } from "./types";
 import {
   buildTree,
   flattenTree,
   getChildCount,
   getProjection,
   removeChildrenOf,
-  setProperty,
 } from "./utilities";
 
 const initialItems: TreeItems = [
@@ -60,16 +48,11 @@ const initialItems: TreeItems = [
   },
 ];
 
-export function SortableTree({}: Props) {
+export function SortableTree() {
   const [items, setItems] = useState(() => initialItems);
   const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
   const [overId, setOverId] = useState<UniqueIdentifier | null>(null);
   const [offsetLeft, setOffsetLeft] = useState(0);
-  const [currentPosition, setCurrentPosition] = useState<{
-    parentId: UniqueIdentifier | null;
-    overId: UniqueIdentifier;
-  } | null>(null);
-
   const flattenedItems = useMemo(() => {
     const flattenedTree = flattenTree(items);
     const collapsedItems = flattenedTree.reduce<string[]>(
@@ -89,19 +72,6 @@ export function SortableTree({}: Props) {
     activeId && overId
       ? getProjection(flattenedItems, activeId, overId, offsetLeft, 50)
       : null;
-  const sensorContext: SensorContext = useRef({
-    items: flattenedItems,
-    offset: offsetLeft,
-  });
-  const [coordinateGetter] = useState(() =>
-    sortableTreeKeyboardCoordinates(sensorContext, false, 50),
-  );
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter,
-    }),
-  );
 
   const sortedIds = useMemo(
     () => flattenedItems.map(({ id }) => id),
@@ -111,41 +81,48 @@ export function SortableTree({}: Props) {
     ? flattenedItems.find(({ id }) => id === activeId)
     : null;
 
-  useEffect(() => {
-    sensorContext.current = {
-      items: flattenedItems,
-      offset: offsetLeft,
-    };
-  }, [flattenedItems, offsetLeft]);
-
   return (
     <DndContext
-      sensors={sensors}
       collisionDetection={closestCenter}
-      measuring={{
-        droppable: {
-          strategy: MeasuringStrategy.Always,
-        },
+      onDragStart={({ active }) => {
+        setActiveId(active.id);
+        setOverId(active.id);
       }}
-      onDragStart={handleDragStart}
-      onDragMove={handleDragMove}
-      onDragOver={handleDragOver}
-      onDragEnd={handleDragEnd}
+      onDragMove={({ delta }) => setOffsetLeft(delta.x)}
+      onDragOver={({ over }) => setOverId(over?.id ?? null)}
+      onDragEnd={({ over, active }) => {
+        setActiveId(null);
+
+        if (projected && over) {
+          const { depth, parentId } = projected;
+          const clonedItems: FlattenedItem[] = JSON.parse(
+            JSON.stringify(flattenTree(items)),
+          );
+          const overIndex = clonedItems.findIndex(({ id }) => id === over.id);
+          const activeIndex = clonedItems.findIndex(
+            ({ id }) => id === active.id,
+          );
+          const activeTreeItem = clonedItems[activeIndex];
+
+          clonedItems[activeIndex] = { ...activeTreeItem, depth, parentId };
+
+          const sortedItems = arrayMove(clonedItems, activeIndex, overIndex);
+          const newItems = buildTree(sortedItems);
+
+          setItems(newItems);
+        }
+      }}
     >
       <SortableContext items={sortedIds} strategy={verticalListSortingStrategy}>
         {flattenedItems.map(({ id, children, collapsed, depth }) => (
           <SortableTreeItem
             key={id}
             id={id}
-            // @ts-ignore
-            value={id}
+            value={String(id)}
             depth={id === activeId && projected ? projected.depth : depth}
             indentationWidth={50}
             indicator={false}
             collapsed={Boolean(collapsed && children.length)}
-            onCollapse={
-              false && children.length ? () => handleCollapse(id) : undefined
-            }
           />
         ))}
         {createPortal(
@@ -166,66 +143,4 @@ export function SortableTree({}: Props) {
       </SortableContext>
     </DndContext>
   );
-
-  function handleDragStart({ active: { id: activeId } }: DragStartEvent) {
-    setActiveId(activeId);
-    setOverId(activeId);
-
-    const activeItem = flattenedItems.find(({ id }) => id === activeId);
-
-    if (activeItem) {
-      setCurrentPosition({
-        parentId: activeItem.parentId,
-        overId: activeId,
-      });
-    }
-
-    document.body.style.setProperty("cursor", "grabbing");
-  }
-
-  function handleDragMove({ delta }: DragMoveEvent) {
-    setOffsetLeft(delta.x);
-  }
-
-  function handleDragOver({ over }: DragOverEvent) {
-    setOverId(over?.id ?? null);
-  }
-
-  function handleDragEnd({ active, over }: DragEndEvent) {
-    resetState();
-
-    if (projected && over) {
-      const { depth, parentId } = projected;
-      const clonedItems: FlattenedItem[] = JSON.parse(
-        JSON.stringify(flattenTree(items)),
-      );
-      const overIndex = clonedItems.findIndex(({ id }) => id === over.id);
-      const activeIndex = clonedItems.findIndex(({ id }) => id === active.id);
-      const activeTreeItem = clonedItems[activeIndex];
-
-      clonedItems[activeIndex] = { ...activeTreeItem, depth, parentId };
-
-      const sortedItems = arrayMove(clonedItems, activeIndex, overIndex);
-      const newItems = buildTree(sortedItems);
-
-      setItems(newItems);
-    }
-  }
-
-  function resetState() {
-    setOverId(null);
-    setActiveId(null);
-    setOffsetLeft(0);
-    setCurrentPosition(null);
-
-    document.body.style.setProperty("cursor", "");
-  }
-
-  function handleCollapse(id: UniqueIdentifier) {
-    setItems((items) =>
-      setProperty(items, id, "collapsed", (value) => {
-        return !value;
-      }),
-    );
-  }
 }
