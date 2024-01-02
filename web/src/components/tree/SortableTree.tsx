9adb3f8e41f7b228795e6262325633d3f10d113b
don.cryptus <don.cryptus@gmail.com>
2024-01-02 21:03:32 +0100

    asd

--- web/src/components/tree/SortableTree.tsx
+++ web/src/components/tree/SortableTree.tsx
@@ -5,12 +5,7 @@ import {
   UniqueIdentifier,
   closestCenter,
 } from "@dnd-kit/core";
-import {
-  SortableContext,
-  arrayMove,
-  useSortable,
-  verticalListSortingStrategy,
-} from "@dnd-kit/sortable";
+import { SortableContext, arrayMove, useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { useMemo, useState } from "react";
 import { useSnapshot } from "valtio";
@@ -38,7 +33,7 @@ function getProjection(
   let projectedDepth = activeItem.depth + dragDepth;
   const { id } = DndStore.parseFlatId(activeId);
 
-  const hasChildren = activeItem?.mealGroup && activeItem.mealGroup.id == id;
+  const hasChildren = activeItem?.mealGroup && activeItem.mealGroup.id == id
 
   if (hasChildren) {
     projectedDepth = Math.min(projectedDepth, 0);
@@ -88,9 +83,7 @@ const SortableTreeItem: React.FC<{
         style={{ transform: CSS.Translate.toString(transform) }}
         {...listeners}
       >
-        {item.group
-          ? `${item.group.name}#${item.group.id}`
-          : `${item.meal?.name}#${item.meal?.id}`}
+        {item.group ? item.group.name : item.meal?.name}
       </div>
     </li>
   );
@@ -126,15 +119,8 @@ export const SortableTree: React.FC = ({}) => {
         setActiveId(active.id);
         setOverId(active.id);
       }}
-      onDragMove={({ delta, over, activatorEvent, collisions }) => {
-        const event = activatorEvent as MouseEvent;
-
-        // console.log(delta, { x: event.clientX, y: event.clientY }, over);
-        setOffsetLeft(delta.x);
-      }}
-      onDragOver={({ over, delta, activatorEvent }) => {
-        setOverId(over?.id);
-      }}
+      onDragMove={({ delta }) => setOffsetLeft(delta.x)}
+      onDragOver={({ over }) => setOverId(over?.id)}
       onDragEnd={({ over, active }) => {
         setActiveId(undefined);
 
@@ -162,56 +148,43 @@ export const SortableTree: React.FC = ({}) => {
       }}
     >
       <div className="flex gap-64">
-        {dndStore.schedules.map((schedule) => {
-          const ids = dndStore.flatSchedules
-            .filter((s) => schedule.servingDate === s.date)
-            .map(({ flatId }) => flatId);
+        {dndStore.schedules.map((schedule) => (
+          <div
+            key={schedule.id}
+            className="border-2 p-2"
+            style={{
+              maxWidth: 600,
+              margin: "0 auto",
+            }}
+          >
+            <SortableContext items={sortedIds}>
+              {dndStore.flatSchedules
+                .filter((s) => schedule.servingDate === s.date)
+                .map((s) => (
+                  <SortableTreeItem
+                    key={s.flatId}
+                    id={s.flatId}
+                    depth={
+                      s.flatId === activeId && projected
+                        ? projected.depth
+                        : s.depth
+                    }
+                    indentationWidth={25}
+                  />
+                ))}
 
-          // console.log(ids);
-          return (
-            <div
-              key={schedule.id}
-              className="border-2 p-2"
-              style={{
-                maxWidth: 300,
-                margin: "0 auto",
-              }}
-            >
-              <div className="text-center font-bold">
-                {schedule.servingDate}
-              </div>
-              <SortableContext
-                items={ids}
-                id={schedule.servingDate}
-                strategy={verticalListSortingStrategy}
-              >
-                {dndStore.flatSchedules
-                  .filter((s) => schedule.servingDate === s.date)
-                  .map((s) => (
-                    <SortableTreeItem
-                      key={s.flatId}
-                      id={s.flatId}
-                      depth={
-                        s.flatId === activeId && projected
-                          ? projected.depth
-                          : s.depth
-                      }
-                      indentationWidth={25}
-                    />
-                  ))}
-              </SortableContext>
-            </div>
-          );
-        })}
-        <DragOverlay>
-          {activeId && activeItem && (
-            <SortableTreeItem
-              id={activeId}
-              depth={activeItem?.depth}
-              indentationWidth={25}
-            />
-          )}
-        </DragOverlay>
+              <DragOverlay>
+                {activeId && activeItem && (
+                  <SortableTreeItem
+                    id={activeId}
+                    depth={activeItem?.depth}
+                    indentationWidth={25}
+                  />
+                )}
+              </DragOverlay>
+            </SortableContext>
+          </div>
+        ))}
       </div>
     </DndContext>
   );
