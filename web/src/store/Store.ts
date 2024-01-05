905096898c98a8cc91d20e55404261e90d498989
don.cryptus <don.cryptus@gmail.com>
2024-01-05 19:42:56 +0100

    cfbc

--- web/src/store/Store.ts
+++ web/src/store/Store.ts
@@ -3,6 +3,68 @@ import { DragEndEvent, DragOverEvent, UniqueIdentifier } from "@dnd-kit/core";
 import { arrayMove } from "@dnd-kit/sortable";
 import { proxy } from "valtio";
 
+export interface DaySchedule {
+  id: UniqueIdentifier;
+  servingDate: string;
+  schedules: Schedule[];
+}
+
+export interface Schedule {
+  id: UniqueIdentifier;
+  meal?: Meal;
+  group?: {
+    id: UniqueIdentifier;
+    name: string;
+    meals: Meal[];
+  };
+}
+
+export interface Meal {
+  id: UniqueIdentifier;
+  name: string;
+}
+
+export const INITIAL_DATAS: DaySchedule[] = [
+  {
+    id: "day1",
+    servingDate: "MONDAY",
+    schedules: [
+      { id: "schedule1", meal: { id: "meal1", name: "pizza" } },
+      { id: "schedule2", meal: { id: "meal2", name: "bread" } },
+      {
+        id: "schedule3",
+        group: {
+          id: "group1",
+          name: "breakfast",
+          meals: [
+            { id: "meal3", name: "tea" },
+            { id: "meal4", name: "sandwich" },
+          ],
+        },
+      },
+    ],
+  },
+  {
+    id: "day2",
+    servingDate: "TUESDAY",
+    schedules: [
+      { id: "schedule4", meal: { id: "meal5", name: "pasta" } },
+      { id: "schedule5", meal: { id: "meal6", name: "salad" } },
+      {
+        id: "schedule6",
+        group: {
+          id: "group2",
+          name: "lunch",
+          meals: [
+            { id: "meal7", name: "soup" },
+            { id: "meal8", name: "burger" },
+          ],
+        },
+      },
+    ],
+  },
+];
+
 interface ItemGroups {
   [key: string]: UniqueIdentifier[];
 }
