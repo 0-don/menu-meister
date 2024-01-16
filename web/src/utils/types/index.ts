44916c9fca8bde473a74bc2a03a9ac54a1b5afab
don.cryptus <don.cryptus@gmail.com>
2024-01-16 23:20:18 +0100

    x

--- web/src/utils/types/index.ts
+++ web/src/utils/types/index.ts
@@ -3,3 +3,9 @@ import { WEEK_DAYS } from "../constants";
 export type WeekDay = (typeof WEEK_DAYS)[number];
 
 export type Size = "sm" | "md" | "lg" | undefined;
+
+export type MenuType = {
+  link: string;
+  name: string;
+  display?: boolean;
+};
