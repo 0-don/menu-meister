3656ee73a29986398d6a334c8411ba31fa0cc545
don.cryptus <don.cryptus@gmail.com>
2024-01-17 18:58:29 +0100

    x

--- web/src/components/table/AddNewRow.tsx
+++ web/src/components/table/AddNewRow.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-
+import { Button } from "@nextui-org/button";
 import { FaRegPlusSquare } from "@react-icons/all-files/fa/FaRegPlusSquare";
+import React from "react";
 
 interface AddNewRowProps {}
 
 export const AddNewRow: React.FC<AddNewRowProps> = ({}) => {
   return (
-    <button className="poin mt-5 flex items-center rounded-full bg-default-100 p-2 hover:bg-default-400">
+    <Button className="mt-5">
       <p className="mr-2">Add new row</p>
       <FaRegPlusSquare />
-    </button>
+    </Button>
   );
 };
