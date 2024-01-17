1d61dd14543facaf084ff5298a9c5ddde536ad66
don.cryptus <don.cryptus@gmail.com>
2024-01-17 18:55:36 +0100

    x

--- web/src/components/table/TableGroupRow.tsx
+++ web/src/components/table/TableGroupRow.tsx
@@ -1,8 +1,9 @@
 import TableStore from "@/store/TableStore";
+import { debounce } from "@/utils/constants";
 import { UniqueIdentifier } from "@dnd-kit/core";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { useSnapshot } from "valtio";
 import { Droppable } from "./Droppable";
 
@@ -12,6 +13,8 @@ interface TableGroupRowProps {
 
 export const TableGroupRow: React.FC<TableGroupRowProps> = ({ id }) => {
   const tableStore = useSnapshot(TableStore);
+  const group = tableStore.getGroup(id)!;
+  const [color, setColor] = useState<string>(group?.color ?? "");
   const {
     attributes,
     listeners,
@@ -22,27 +25,33 @@ export const TableGroupRow: React.FC<TableGroupRowProps> = ({ id }) => {
     isDragging,
   } = useSortable({ id });
 
-  const group = tableStore.getGroup(id);
-
-  if (!group) return null;
+  const debouncedSetColor = useCallback(
+    debounce((newColor: string) => setColor(newColor), 0),
+    [],
+  );
 
   return (
     <section
-      className={`${isDragging ? "relative z-50" : ""} grid grid-cols-8 gap-2 p-2`}
+      className={`${isDragging ? "relative z-50" : ""} grid grid-cols-8 gap-2 p-2 focus:outline-none`}
       style={{
         transform: CSS.Transform.toString(transform),
         transition,
-        borderLeft: `5px solid ${group.color}`,
+        borderLeft: `5px solid ${color}`,
       }}
       {...attributes}
       ref={setNodeRef}
     >
       <div
         className="mr-5 p-5"
-        style={{ color: group.color! }}
+        style={{ color }}
         {...listeners}
         ref={setActivatorNodeRef}
       >
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => debouncedSetColor(e.target.value)}
+        />
         {group.name}
       </div>
       <Droppable day="monday" group={group.id} />
