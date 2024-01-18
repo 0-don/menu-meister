7e7c69aabc90fcd3560d99105079fb86125bf5c5
don.cryptus <don.cryptus@gmail.com>
2024-01-18 01:00:21 +0100

    ok

--- web/src/app/[locale]/(main)/@dashboard/page.tsx
+++ web/src/app/[locale]/(main)/@dashboard/page.tsx
@@ -1,20 +1,13 @@
 "use client";
 
 import { MainTable } from "@/components/MainTable";
-import { DashboardPagination } from "@/components/pages/dashboard/table/DashboardPagination";
+import { DashboardPagination } from "@/components/pages/dashboard/utils/DashboardPagination";
 
 interface DashboardPageProps {}
 
 export default function DashboardPage({}: DashboardPageProps) {
-  // const { data: { getAllMealSchedulesAdmin } = {} } = useGqlQuery(
-  //   GET_ALL_MEAL_SCHEDULES_ADMIN,
-  //   { where: {} },
-  // );
-
   return (
     <>
-      {/* <Test /> */}
-
       <DashboardPagination />
       <MainTable />
     </>
