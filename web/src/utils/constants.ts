0052629557d2bfdd3a7fcb2c3004b5e4f2b37443
don.cryptus <don.cryptus@gmail.com>
2024-01-22 00:41:24 +0100

    ok

--- web/src/utils/constants.ts
+++ web/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const X_URL = "x-url";
-export const BACKEND_INTERNAL_URL = "http://meal-manager-server:4000/graphql";
+export const BACKEND_INTERNAL_URL = "http://menu-meister-server:4000/graphql";
 export const TOKEN = "token";
 export const ROLES = "roles";
 
