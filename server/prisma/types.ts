5bae11c482daaa4329a1b334cf98b38087b338ec
don.cryptus <don.cryptus@gmail.com>
2024-01-21 03:41:53 +0100

    easy

---
+++ server/prisma/types.ts
@@ -0,0 +1,21 @@
+export interface Ingredient {
+  allergens: string[];
+  additives: string[];
+  kitchens: any[];
+  properties: string[];
+  name: string;
+  categories: any[];
+  seasons: any[];
+  food_forms: any[];
+  bls_identifier: string;
+  energyKcal: number;
+  energyKj: number;
+  breadUnits: number;
+  carbohydrates: number;
+  sugars: number;
+  salt: number;
+  fats: number;
+  saturatedFats: number;
+  unsaturatedFats: number;
+  protein: number;
+}
