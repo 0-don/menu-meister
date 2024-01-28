637600b087fd8aa2d87f6869d825bfecddf766d6
don.cryptus <don.cryptus@gmail.com>
2024-01-28 22:05:41 +0100

    x

--- web/src/documents/fragments/dashboard/index.ts
+++ web/src/documents/fragments/dashboard/index.ts
@@ -14,7 +14,6 @@ export const WEEKLY_MEAL_GROUP_FRAGMENT = graphql(/* GraphQL */ `
       name
       description
       image
-      imageName
     }
     tuesdayMeal {
       id
@@ -27,35 +26,30 @@ export const WEEKLY_MEAL_GROUP_FRAGMENT = graphql(/* GraphQL */ `
       name
       description
       image
-      imageName
     }
     thursdayMeal {
       id
       name
       description
       image
-      imageName
     }
     fridayMeal {
       id
       name
       description
       image
-      imageName
     }
     saturdayMeal {
       id
       name
       description
       image
-      imageName
     }
     sundayMeal {
       id
       name
       description
       image
-      imageName
     }
   }
 `);
