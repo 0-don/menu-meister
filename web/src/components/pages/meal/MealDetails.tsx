3a80323e318ff908e5b80b5104a456e4b6d66e3e
don.cryptus <don.cryptus@gmail.com>
2024-01-28 14:33:09 +0100

    x

--- web/src/components/pages/meal/MealDetails.tsx
+++ web/src/components/pages/meal/MealDetails.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { GET_MEAL_ADMIN } from "@/documents/query/meal";
+import { useGqlQuery } from "@/fetcher";
 import React from "react";
 
 interface MealDetailsProps {
@@ -8,8 +10,16 @@ interface MealDetailsProps {
 }
 
 export const MealDetails: React.FC<MealDetailsProps> = ({ id, modal }) => {
+  const { data: { getMealAdmin } = {} } = useGqlQuery(GET_MEAL_ADMIN, {
+    where: { id: { equals: id } },
+  });
+
+  console.log(getMealAdmin);
+
   return (
-    <div className={`w-full rounded-lg bg-content1 p-5 ${modal ? "" : "mt-5 container mx-auto"}`}>
+    <div
+      className={`w-full rounded-lg bg-content1 p-5 ${modal ? "" : "container mx-auto mt-5"}`}
+    >
       <h1>asdasd {id}</h1>
       <h1>asdasd {id}</h1>
       <h1>asdasd {id}</h1>
---
+++ web/src/documents/query/meal/index.ts
@@ -0,0 +1,135 @@
+import { graphql } from "@/gql";
+
+export const GET_MEAL_ADMIN = graphql(/* GraphQL */ `
+  query GetMealAdmin(
+    $where: MealWhereInput
+    $orderBy: [MealOrderByWithRelationInput!]
+    $cursor: MealWhereUniqueInput
+    $take: Int
+    $skip: Int
+    $distinct: [MealScalarFieldEnum!]
+  ) {
+    getMealAdmin(
+      where: $where
+      orderBy: $orderBy
+      cursor: $cursor
+      take: $take
+      skip: $skip
+      distinct: $distinct
+    ) {
+      id
+      name
+      description
+      image
+      createdAt
+      updatedAt
+      allergens {
+        id
+        name
+      }
+      additives {
+        id
+        name
+      }
+      properties {
+        id
+        name
+      }
+      categories {
+        id
+        name
+      }
+      seasons {
+        id
+        name
+      }
+      foodForms {
+        id
+        name
+      }
+      mealRecipe {
+        id
+        recipe {
+          id
+          name
+          allergens {
+            id
+            name
+          }
+          additives {
+            id
+            name
+          }
+          properties {
+            id
+            name
+          }
+          categories {
+            id
+            name
+          }
+          seasons {
+            id
+            name
+          }
+          foodForms {
+            id
+            name
+          }
+          kitchens {
+            id
+            name
+          }
+          recipeIngredient {
+            id
+            amount
+            unit
+            ingredient {
+              id
+              name
+              blsIdentifier
+              energyKcal
+              energyKj
+              breadUnits
+              carbohydrates
+              sugars
+              salt
+              fats
+              unsaturatedFats
+              saturatedFats
+              protein
+              allergens {
+                id
+                name
+              }
+              additives {
+                id
+                name
+              }
+              properties {
+                id
+                name
+              }
+              categories {
+                id
+                name
+              }
+              seasons {
+                id
+                name
+              }
+              foodForms {
+                id
+                name
+              }
+              kitchens {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`);
