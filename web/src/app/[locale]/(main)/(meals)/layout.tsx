c8620aa787fa9ebc6b6e120713a527e5503bb886
don.cryptus <don.cryptus@gmail.com>
2024-02-08 16:30:33 +0100

    asd

--- web/src/app/[locale]/(main)/(meals)/layout.tsx
+++ web/src/app/[locale]/(main)/(meals)/layout.tsx
@@ -22,7 +22,7 @@ export default async function MealsLayout({
     { document: GET_SETTINGS_USER },
   ]);
   return (
-    <NextIntlProvider tree={["Meals"]}>
+    <NextIntlProvider tree={["Meals", "Enums"]}>
       <HydrationBoundary state={state}>
         {children}
         {mealModal}
