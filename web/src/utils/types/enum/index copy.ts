4622a85666f9afbf465186acc458c83bfa438c01
don.cryptus <don.cryptus@gmail.com>
2024-02-26 12:58:02 +0100

    easy

---
+++ web/src/utils/types/enum/index copy.ts
@@ -0,0 +1,45 @@
+type TimeOfDayUnion =
+  | "Any"
+  | "Morning"
+  | "Forenoon"
+  | "Noon"
+  | "Afternoon"
+  | "Evening"
+  | "Night";
+
+type UnionToIntersection<U> = (
+  U extends never ? never : (arg: U) => never
+) extends (arg: infer I) => void
+  ? I
+  : never;
+type UnionToTuple<T, A extends any[] = []> =
+  UnionToIntersection<T extends never ? never : (t: T) => T> extends (
+    _: never,
+  ) => infer W
+    ? UnionToTuple<Exclude<T, W>, [...A, W]>
+    : A;
+
+function createEnum<T extends string>(keys: UnionToTuple<T>): { [K in T]: K } {
+  const tupleArray = keys as unknown as T[];
+
+  return tupleArray.reduce(
+    (acc, key) => {
+      acc[key] = key;
+      return acc;
+    },
+    {} as { [K in T]: K },
+  );
+}
+
+export const TimeOfDay = createEnum<TimeOfDayUnion>([
+  "Night",
+  "Evening",
+  "Afternoon",
+  "Noon",
+  "Forenoon",
+  "Morning",
+  "Any",
+]);
+
+console.log(TimeOfDay);
+type test = keyof typeof TimeOfDay;
--- web/src/utils/types/enum/index.ts
+++ web/src/utils/types/enum/index.ts
@@ -1,26 +1,23 @@
 import { graphql } from "gql.tada";
 
-type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
-  k: infer I,
-) => void
+type UnionToIntersection<U> = (
+  U extends never ? never : (arg: U) => never
+) extends (arg: infer I) => void
   ? I
   : never;
-type LastOf<T> =
-  UnionToIntersection<T extends any ? () => T : never> extends () => infer R
-    ? R
-    : never;
+type UnionToTuple<T, A extends any[] = []> =
+  UnionToIntersection<T extends never ? never : (t: T) => T> extends (
+    _: never,
+  ) => infer W
+    ? UnionToTuple<Exclude<T, W>, [...A, W]>
+    : A;
 
-type Push<T extends any[], V> = [...T, V];
-type TuplifyUnion<
-  T,
-  L = LastOf<T>,
-  N = [T] extends [never] ? true : false,
-> = true extends N ? [] : Push<TuplifyUnion<Exclude<T, L>>, L>;
+function createEnum<T extends string>(keys: UnionToTuple<T>): { [K in T]: K } {
+  const tupleArray = keys as unknown as T[];
 
-function createEnum<T extends string>(keys: TuplifyUnion<T>): { [K in T]: K } {
-  return keys.reduce(
+  return tupleArray.reduce(
     (acc, key) => {
-      (acc as any)[key] = key;
+      acc[key] = key;
       return acc;
     },
     {} as { [K in T]: K },
@@ -29,32 +26,32 @@ function createEnum<T extends string>(keys: TuplifyUnion<T>): { [K in T]: K } {
 
 export const TimeOfDay = createEnum<
   ReturnType<typeof graphql.scalar<"TimeOfDay">>
->(["Any", "Morning", "Forenoon", "Noon", "Afternoon", "Evening", "Night"]);
+>(["Night", "Evening", "Afternoon", "Noon", "Forenoon", "Morning", "Any"]);
 
 export const UserRoleName = createEnum<
   ReturnType<typeof graphql.scalar<"UserRoleName">>
->(["User", "Admin", "Mod", "Guest"]);
+>(["Guest", "Mod", "Admin", "User"]);
 
 export const MealLocation = createEnum<
   ReturnType<typeof graphql.scalar<"MealLocation">>
 >([
-  "InRoom",
-  "CommunalDining",
-  "OutdoorPatio",
-  "TherapyGarden",
-  "SpecialDietary",
-  "StaffCafeteria",
-  "VisitorCafe",
-  "RehabilitationGym",
-  "GroupActivityRoom",
-  "QuietRoom",
-  "NutritionCenter",
-  "Poolside",
-  "FamilyDining",
-  "HealingGarden",
   "OnTheGo",
+  "HealingGarden",
+  "FamilyDining",
+  "Poolside",
+  "NutritionCenter",
+  "QuietRoom",
+  "GroupActivityRoom",
+  "RehabilitationGym",
+  "VisitorCafe",
+  "StaffCafeteria",
+  "SpecialDietary",
+  "TherapyGarden",
+  "OutdoorPatio",
+  "CommunalDining",
+  "InRoom",
 ]);
 
 export const MealType = createEnum<
   ReturnType<typeof graphql.scalar<"SortOrder">>
->(["asc", "desc"]);
+>(["desc", "asc"]);
